195913fe330459498759e3d12224e9175c381102
bagasadit123 <bagasadit430@gmail.com>
2026-03-03 11:46:05 +0700

    display nya

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -57,3 +57,25 @@ void bubbleSortArray()
         cout << endl;
     } while (pass <= n - 1); 
 }
+void display()
+{
+    cout << endl;
+    cout << "==================================" << endl;
+    cout << "Element Arrar yang telah tersusun" << endl;
+    cout << "==================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j];
+        if (j < n -1)
+        {
+            cout << " --> ";
+        }
+    } 
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
